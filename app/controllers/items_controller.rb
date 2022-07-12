55c4f03be46c77fe321cb79ae58e66f7b42c37c9
jangojanga <shibuyajango0523@gmail.com>
2022-07-12 16:35:57 +0900

    ４

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,16 +1,8 @@
 class ItemsController < ApplicationController
-
-
-
   # def destroy
   #   item = Item.find(params[:id])
   #   if item.destroy
   #     redirect_to root_path
   #   end
   # end
-
-
-
-
-
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,33 +3,23 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         has_many :items
-         has_many :buyers
+  has_many :items
+  has_many :buyers
 
-        
+  validates :name, presence: true
+  validates :first_name, presence: true
+  validates :first_name_huri, presence: true
+  validates :family_name, presence: true
+  validates :family_name_huri, presence: true
+  validates :birthday, presence: true
 
+  validates :family_name_huri, format: { with: /\A[\p{katakana} ー－&&[^ -~｡-ﾟ]]+\z/ }
+  validates :first_name_huri, format: { with: /\A[\p{katakana} ー－&&[^ -~｡-ﾟ]]+\z/ }
 
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
 
-         validates :name, presence: true
-         validates :first_name, presence: true
-         validates :first_name_huri, presence: true
-         validates :family_name, presence: true
-         validates :family_name_huri, presence: true
-         validates :birthday, presence: true
+  # validates :password, format: { with:/^[a-zA-Z0-9]+$/}
 
-        validates :family_name_huri, format: { with: /\A[\p{katakana} ー－&&[^ -~｡-ﾟ]]+\z/}
-        validates :first_name_huri, format: { with: /\A[\p{katakana} ー－&&[^ -~｡-ﾟ]]+\z/}
-
-        validates :password, format: { with:/\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}
-
-        # validates :password, format: { with:/^[a-zA-Z0-9]+$/}
-
-         validates :family_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
-         validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
-
-
-
-
-
-
+  validates :family_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+  validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
 end
