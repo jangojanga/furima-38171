1a123151175bee41df0a95dd9b523857b3b5a44f
jangojanga <shibuyajango0523@gmail.com>
2022-07-17 17:32:01 +0900

    修正３

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,11 +4,11 @@ class Item < ApplicationRecord
 
   has_one_attached :image
 
-  has_one :genre
-  has_one :status
-  has_one :lord
-  has_one :area
-  has_one :days
+  has_many :genres
+  has_many :statuss
+  has_many :lords
+  has_many :areas
+  has_many :dayss
 
   validates :image, presence: true
   validates :name, presence: true
@@ -18,9 +18,9 @@ class Item < ApplicationRecord
   validates :lord_id, presence: true
   validates :former_area_id, presence: true
   validates :days_before_id, presence: true
-  validates :price, presence: true, numericality: {only_integer: true},format:{ greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },
+  validates :price, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },
   format: { with: /\A[0-9]+\z/ }
-
+ 
 
   
                   validates :category_id, numericality: { greater_than_or_equal_to: 2, less_than_or_equal_to: 21 }
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -31,31 +31,31 @@ RSpec.describe Item, type: :model do
         end
 
         it "商品の詳細カテゴリが空では登録できない" do
-          @item.category_id = 0
+          @item.category_id = 1
           @item.valid?
           expect(@item.errors.full_messages).to include("Category must be greater than or equal to 2")
         end
 
         it "商品の詳細商品の状態が空では登録できない" do
-          @item.item_status_id = 0
+          @item.item_status_id = 1
           @item.valid?
           expect(@item.errors.full_messages).to include("Item status must be greater than or equal to 2")
         end
 
         it "配送料の負担が空では登録できない" do
-          @item.lord_id = 0
+          @item.lord_id = 1
           @item.valid?
           expect(@item.errors.full_messages).to include("Lord must be greater than or equal to 2")
         end
 
         it "配送元の地位域が空では登録できない" do
-          @item.former_area_id = 0
+          @item.former_area_id = -1
           @item.valid?
           expect(@item.errors.full_messages).to include("Former area must be greater than or equal to 1")
         end
 
         it "発送までの日数が空では登録できない" do
-          @item.days_before_id = 0
+          @item.days_before_id = 1
           @item.valid?
           expect(@item.errors.full_messages).to include("Days before must be greater than or equal to 2")
         end
