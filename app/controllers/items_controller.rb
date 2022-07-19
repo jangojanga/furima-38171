f63d6ba33bbe1d7010507281b7b2509a58578247
jangojanga <shibuyajango0523@gmail.com>
2022-07-19 18:34:03 +0900

    とりま完成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,12 @@ class ItemsController < ApplicationController
 
   before_action :authenticate_user!, except: [:index]
 
+  def index
+    @items = Item.all
+
+  end
+
+
   def new
     @item = Item.new
   end
