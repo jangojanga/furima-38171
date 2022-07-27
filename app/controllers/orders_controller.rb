b80bc1e7cade1fd883c5c4a842204229d7de7fa5
jangojanga <shibuyajango0523@gmail.com>
2022-07-27 21:50:17 +0900

    修正８

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,19 +1,26 @@
 class OrdersController < ApplicationController
-  before_action :set_item, only: [:index, :create]
 
-  before_action :authenticate_user!, only: [:index, :create]
+  before_action :set_item, only: [:index, :create ]
 
-  def index
-    @ab = Ab.new
+  before_action :authenticate_user!,  only: [:index, :create]
 
-    redirect_to root_path if current_user.id == @item.user.id || !@item.buyer.nil?
+def index
+  @ab = Ab.new
+
+  if current_user.id == @item.user.id || @item.buyer != nil
+    redirect_to root_path
   end
 
-  def create
-    # binding.pryは処理に不要なので削除する
-    # 値をDBへ保存する実装
 
-    @ab = Ab.new(ab_params)
+end
+
+
+
+def create
+  # binding.pryは処理に不要なので削除する
+  # 値をDBへ保存する実装
+
+  @ab = Ab.new(ab_params)
     if @ab.valid?
       pay_item
       @ab.save
@@ -22,26 +29,29 @@ class OrdersController < ApplicationController
     else
       render :index
     end
-  end
+end
 
-  private
+
+private
 
   def ab_params
-    params.require(:ab).permit(:tel, :post_code, :former_area_id, :city, :address, :building, :price).merge(
-      user_id: current_user.id, item_id: params[:item_id], token: params[:token]
-    )
+    params.require(:ab).permit(:tel,:post_code, :former_area_id, :city, :address, :building, :price).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
 
   def set_item
     @item = Item.find(params[:item_id])
   end
 
+
   def pay_item
-    Payjp.api_key = ENV['PAYJP_SECRET_KEY'] # 自身のPAY.JPテスト
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]  # 自身のPAY.JPテスト
     Payjp::Charge.create(
-      amount: @item.price, # 商品の値段
-      card: @ab.token, # カードトークン
+      amount: @item.price,  # 商品の値段
+      card: @ab.token,    # カードトークン
       currency: 'jpy'                 # 通貨の種類（日本円）
     )
   end
+
 end
+
+
